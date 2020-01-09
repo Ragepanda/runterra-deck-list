74a0956c75023329bab4adc6ba170d06ac1cc08c
ragepanda <jwoltz13@gmail.com>
2020-01-09 16:10:12 -0500

    card and list merge

--- client/src/pages/card/card.js
+++ client/src/pages/card/card.js
@@ -1,82 +1,109 @@
 import React from "react";
 import baseSet from "../../card_info/set1.json"
-// import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet";
 class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      card: {},
+      isLoaded: false
+    };
+  }
 
-  componentDidMount(){
-    console.log(this.props.match.params.name);
+
+  componentDidMount() {
+    var cardName = this.props.match.params.name.replace(/_/g, " ");
+
+    console.log(cardName);
     console.log(baseSet);
+
+    for (var index = 0; index < baseSet.length; index++) {
+      var searchCard = baseSet[index];
+
+      if (searchCard.name === cardName) {
+        this.setState({ card: searchCard, isLoaded: true });
+        console.log("Card matched");
+        console.log(baseSet[index]);
+        break;
+      }
+    }
   }
   render() {
-    var card = {name: "Name", cost: "Cost", region: "Region", type: "Type", subtype: "Subtype", attack: "Attack", health: "Health", 
-    rarity: "Rarity", description: "Description", flavorText: "Flavor Text", keywords: "Keywork" }
-    return (
-      <div>                                                            
-        {/* // <Helmet>
-        //   <title>{"card.name" + " |  Legends of Runeterra Cards"}</title>
-        //   <meta name="description" content={card.name + " is one of the many cards in Legends of Runeterra. This page will help you evaluate " +card.name+" as well as other Legends of Runeterra cards." } />
-        //   <meta name="keywords" content={"Legends,Runeterra, cards, lor, decks, best, card, library, list"} />
-        //   <meta name="author" content="runeterrahub.com" />
-        //   <meta http-equiv="Content-Language" content="en-US" />
-        //   <meta name="rating" content="kids" />
-        //   <meta http-equiv="content-type" content="text/html" charSet="utf-8" />
-        // </Helmet>                             */}
-        <div className="cardName">                
-          <h2>{card.name}</h2>
-        </div>
 
+    if (this.state.isLoaded) {
+      var card = this.state.card;
+      return (
         <div>
-           <img className="cardImg" src={""} alt={card.name}/>
-        </div>
+           <Helmet>
+           <title>{"card.name" + " |  Legends of Runeterra Cards"}</title>
+           <meta name="description" content={card.name + " is one of the many cards in Legends of Runeterra. This page will help you evaluate " +card.name+" as well as other Legends of Runeterra cards." } />
+           <meta name="keywords" content={"Legends,Runeterra, cards, lor, decks, best, card, library, list"} />
+           <meta name="author" content="runeterrahub.com" />
+           <meta http-equiv="Content-Language" content="en-US" />
+           <meta name="rating" content="kids" />
+           <meta http-equiv="content-type" content="text/html" charSet="utf-8" />
+         </Helmet>                            
+          <div className="cardName">
+            <h2>{card.name}</h2>
+          </div>
 
-        <div className="cardSpecs">
-          <div className="row">
-            <div>Name</div>
-            <div>{card.name}</div>
+          <div>
+            <img className="cardImg" src={""} alt={card.name} />
           </div>
-          <div className="row">
-            <div>Mana Cost</div>
-            <div>{card.cost}</div>
-          </div>
-          <div className="row">
-            <div>Faction</div>
-            <div>{card.region}</div>
-          </div>
-          <div className="row">
-            <div>Card Type</div>
-            <div>{card.type} - {card.subtype}</div>
-          </div>
-          <div className="row">
-            <div>Attack / Health</div>
-            <div>{card.attack} / {card.health}</div>
-          </div>
-          <div className="row">
-            <div>Rarity</div>
-            <div>{card.rarity}</div>
-          </div>
-          <div className="row">
-            <div>Text</div>
-            <div>{card.description}</div>
-          </div>
-          <div className="row">
-            <div>Flavor Text</div>
-            <div>{card.flavorText}</div>
-          </div>
-          <div className="row">
-            <div>Keywords</div>
-            <div>{card.keywords}</div> 
-          </div>
-          <div className="row">
-            <div>Decks with this Card</div>
-            <div>CLICK HERE FOR DECKS WITH THIS CARD</div> 
-          </div>
-          <div className="row">
-            <div>Card Evaluation</div>
-            <div>Data on how card fares in diff types of matches</div> 
+
+          <div className="cardSpecs">
+            <div className="row">
+              <div>Name</div>
+              <div>{card.name}</div>
+            </div>
+            <div className="row">
+              <div>Mana Cost</div>
+              <div>{card.cost}</div>
+            </div>
+            <div className="row">
+              <div>Faction</div>
+              <div>{card.region}</div>
+            </div>
+            <div className="row">
+              <div>Card Type</div>
+              <div>{card.type} - {card.subtype}</div>
+            </div>
+            <div className="row">
+              <div>Attack / Health</div>
+              <div>{card.attack} / {card.health}</div>
+            </div>
+            <div className="row">
+              <div>Rarity</div>
+              <div>{card.rarity}</div>
+            </div>
+            <div className="row">
+              <div>Text</div>
+              <div>{card.description}</div>
+            </div>
+            <div className="row">
+              <div>Flavor Text</div>
+              <div>{card.flavorText}</div>
+            </div>
+            <div className="row">
+              <div>Keywords</div>
+              <div>{card.keywords}</div>
+            </div>
+            <div className="row">
+              <div>Decks with this Card</div>
+              <div>CLICK HERE FOR DECKS WITH THIS CARD</div>
+            </div>
+            <div className="row">
+              <div>Card Evaluation</div>
+              <div>Data on how card fares in diff types of matches</div>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+
+    else{
+      return(<div>Loading... </div>);
+    }
   }
 };
 

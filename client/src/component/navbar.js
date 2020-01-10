393e864d20ea623fdad9fe0a54cacf0cf1bf1cd6
dptheodore <dptheodore93@gmail.com>
2020-01-10 15:26:58 -0500

    hover navbar

--- client/src/component/navbar.js
+++ client/src/component/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
-// import './NavBar.css';
+import './navbar.css';
 class Navbar extends React.Component {
     constructor(props) {
         super(props); // ✅ We passed props
@@ -11,33 +11,39 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <a class="navbar-brand" href="#">Navbar</a>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+              <a class="navbar-brand" href="/">Runeterra Hub</a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Meta</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link disabled" href="#">Decks</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Deckbuilder</a>
                   </li>
                   <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Dropdown
+                      Card Library
                     </a>
-                    <div class="dropdown-menu" id="ass" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="/set">Base Set</a>
                     </div>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link disabled" href="#">Login</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Register</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Hub Tracker</a>
                   </li>
                 </ul>
               </div>

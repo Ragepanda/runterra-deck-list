65414c404164c7f065eb113f62c1a8c12a655e6b
ragepanda <jwoltz13@gmail.com>
2020-01-13 13:09:03 -0500

    navbar included

--- client/src/component/navbar.js
+++ client/src/component/navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
-import './navbar.css';
+import './Navbar.css';
 class Navbar extends React.Component {
     constructor(props) {
         super(props); // ✅ We passed props
       }
-
+//navbar
     render() {
         return (
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">

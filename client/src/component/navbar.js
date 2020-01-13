92d4199d3043d3cdce13e8ed410e9b1700c8eb61
dptheodore <dptheodore93@gmail.com>
2020-01-12 23:58:42 -0500

    added a fixed top navbar

--- client/src/component/navbar.js
+++ client/src/component/navbar.js
@@ -8,7 +8,7 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
               <a className="navbar-brand" href="/">Runeterra Hub</a>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>

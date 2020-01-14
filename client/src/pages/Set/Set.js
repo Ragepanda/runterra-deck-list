8924e0ce9b5e7184731e0d9738acb477157c090e
ragepanda <jwoltz13@gmail.com>
2020-01-14 14:33:43 -0500

    reduced picture file name changed

--- client/src/pages/Set/Set.js
+++ client/src/pages/Set/Set.js
@@ -27,18 +27,10 @@ class Set extends React.Component {
   }
 
   createRows() {
-<<<<<<< HEAD
-    const set = baseSet;
-    const list = set.map((card, index) =>
-        <div className="col-6 col-sm-6 col-md-4 col-lg-3 p-3" key={index}>
-          <a href={"/card/"+card.name.replace(/ /g, "_").replace(/:/g,"")}><img className="image-container img-fluid"  src={"/img/cards/"+card.cardCode+".png"} alt={card.name} /></a>
-          <div className="text-center pb-3"><a href={"/card/"+card.name.replace(/ /g, "_").replace(/:/g,"")}>{card.name}</a></div>
-=======
     const list = baseSet.map((card, index) => {
       if(card.rarity != "None")
         return <div className="col-6 col-sm-6 col-md-4 col-lg-3 p-3" key={index}>
           <a href={"/card/"+card.name.replace(/ /g, "_").replace(/:/g,"")}><img className="image-container img-fluid" src={"/img/cards/"+card.cardCode+".png"} alt={"Legends of Runeterra Cards " + card.name} /></a>
->>>>>>> master
         </div>
       else
         return " "

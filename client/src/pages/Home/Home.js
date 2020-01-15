1106990fc8dc9fc266011de6a58c730996592d93
dptheodore <dptheodore93@gmail.com>
2020-01-15 13:12:06 -0500

    changed helmet and added content to home pg

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -30,15 +30,15 @@ class Home extends React.Component{
 
       createHelmet(){
 
-            let metatitle = "Runeterra Hub | Legends of Runeterra Decks, Cards, Guides and Community";
-            let descrip = "Runeterra Hub is your spot for Legends of Runeterra decks, Legends of Runeterra cards, and Legends of Runeterra guides.";
-            let metacontent = "cards, hub, card library, lor, legend, legends, runeterra, deck, decklist, decklists, decks, set, sets, spell, spells, community, guides, guide, faction, factions, champion, champions";
+            let metatitle = "Runeterra Nexus | Legends of Runeterra Decks, Cards, Guides and Community";
+            let descrip = "Runeterra Nexus is your spot for Legends of Runeterra decks, Legends of Runeterra cards, and Legends of Runeterra guides.";
+            let metacontent = "cards, nexus, card library, lor, legend, legends, runeterra, deck, decklist, decklists, decks, set, sets, spell, spells, community, guides, guide, faction, factions, champion, champions";
 
             let helmet = <Helmet>
                 <title>{metatitle}</title>
                 <meta name="description" content={descrip}/>
                 <meta name="keywords" content={metacontent} />
-                <meta name="author" content="runeterrahub.com"/>
+                <meta name="author" content="runeterranexus.com"/>
                 <meta http-equiv="Content-Language" content="en-US"/>
                 <meta name="rating" content="kids"/>
                 <meta http-equiv="content-type" content="text/html" charSet="utf-8" />
@@ -63,11 +63,11 @@ class Home extends React.Component{
           return (
             <div className="Home">
                 {this.createHelmet()}
-                <div className="setName text-center pt-4"><h2>Runeterra Hub</h2></div>
-                <div className="setName text-center pb-5 pt-1"><p>Runeterra Hub is your spot for Legends of Runeterra decks, Legends of Runeterra cards, and Legends of Runeterra guides.</p></div>
+                <div className="setName text-center pt-4"><h2>Runeterra Nexus</h2></div>
+                <div className="setName text-center pb-5 pt-1"><p>Runeterra Nexus is your spot for Legends of Runeterra decks, Legends of Runeterra cards, and Legends of Runeterra guides.</p></div>
 
                 <p>
-                  Here is our API being passed: {this.state.items.express}
+                  If you love Legends of Runeterra and love staying up to date on the best Legends of Runeterra decks, make sure to stay tuned for content from Runeterra Nexus. We are your source for all new Legends of Runeterra Cards as well as the always evolving Legends of Runeterra meta. Stay tuned for our take on the best Legends of Runeterra beta decks. 
                 </p>
                 
   

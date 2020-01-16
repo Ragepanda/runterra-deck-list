f656b3ba1dbbf2c453251ab607d9cc6eedf787fe
dptheodore <dptheodore93@gmail.com>
2020-01-16 13:53:41 -0500

    more horniness removed

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="App" id="element-with-background-image">
+        <div className="App" >
           <Navbar/>
           <Switch>
             <Route exact path="/" component={Home} />

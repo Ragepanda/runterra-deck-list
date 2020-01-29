4a8941bcf5585ae3e378ae479768e2e4a76d0877
dptheodore <dptheodore93@gmail.com>
2020-01-29 15:23:05 -0500

    changed gitignore

---
+++ client/build/precache-manifest.514f1e25f691e89ef467f5dd17cba905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c2c401b707f156731f79a231a1ab2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d6ca72c63ad55c36456",
+    "url": "/static/css/main.557d47b8.chunk.css"
+  },
+  {
+    "revision": "471b139445dcb98c93c1",
+    "url": "/static/js/2.3f10791c.chunk.js"
+  },
+  {
+    "revision": "070070a623b0d78c1a86bcdb00cc1f71",
+    "url": "/static/js/2.3f10791c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d6ca72c63ad55c36456",
+    "url": "/static/js/main.73d9bc30.chunk.js"
+  },
+  {
+    "revision": "9d6a3157520b5bdc9803",
+    "url": "/static/js/runtime-main.b4270a88.js"
+  }
+]);

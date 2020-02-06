e1f190e3391998ab12aa7cf68d0ecde54edc25be
ragepanda <jwoltz13@gmail.com>
2020-02-05 19:13:01 -0500

    login navbar and profile page implemented

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import Articles from "./pages/Articles/Articles";
 import Privacy from "./pages/Privacy/Privacy";
 import Terms from "./pages/Terms/Terms";
 import About from "./pages/About/About";
-
+import Profile from "./pages/Profile/Profile";
 
 import Navbar from "./component/Navbar";
 import cookieSession from "cookie-session";
@@ -39,6 +39,7 @@ class App extends React.Component {
             <Route exact path="/deck_lists/" component={Decklists} />
             <Route exact path="/deck_lists/:deckName/:id" component={Deck} />
             <Route exact path="/deck_builder" component={Deckbuilder} />
+            <Route exact path="/profile" component={Profile} />
             <Route exact path="/articles/:article/:id" component={Article} />
             <Route exact path="/articles" component={Articles}/>
             <Route exact path="/privacy" component={Privacy} />
